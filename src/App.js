da7c72c0e511b1b16bcc69f0a8f43c4129dac9c5
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-15 14:25:59 +1030

    learn components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name="Hamish" age={14} email="hamish@email.com" />
+      <User name="Lucy" age={15} email="lucy@email.com" />
+      <User name="Beth" age={13} email="beth@email.com" />
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+};
 
 export default App;
