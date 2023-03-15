659055ca9d05cd990918f433b620fbee670acd55
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-15 18:23:17 +1030

    Working with lists

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
 import './App.css';
 
 function App() {
-  const age = 14;
-  const isGreen = true;
+  const names = ["Dan", "Dill", "Jackson", "Adam", "Tom", "Terry"];
 
-  return (
+
+   return (
     <div className="App">
-      {age >= 18 ? <h1>OVERAGE</h1> : <h1>UNDERAGE</h1>}
-      <h1 style={{color: isGreen ? "green" : "red"}}>This has color</h1>
-
-      {isGreen && <button>Here's a button</button>}
+      {names.map((name, key) => {
+        return <h1 key={key}> {name} </h1>;
+      })}
     </div>
-  )
+   );
 }
 
   export default App;
-
-
-
-
-  // //   return (
-  // //     <div className="App">
-  // //       <User name="Hamish" age={14} email="hamish@email.com" />
-  // //       <User name="Lucy" age={15} email="lucy@email.com" />
-  // //       <User name="Beth" age={13} email="beth@email.com" />
-  // //     </div>
-  // //   );
-  // // }
-
-  // // const User = (props) => {
-  // //   return (
-  // //     <div>
-  // //       <h1>{props.name}</h1>
-  // //       <h1>{props.age}</h1>
-  // //       <h1>{props.email}</h1>
-  // //     </div>
-  // //   );
-
-  //   return (
-  //     <div className="App">
-  //       <Job salary={90000} position="Senior SDE" company="Amazon" />
-  //       <Job salary={12000} position="Junior SDE" company="Google" />
-  //       <Job salary={10000} position="Project Manager" company="Netflix" />
-  //     </div>
-  //   );
-  // };
-
-  // const Job = (props) => {
-  //   return (
-  //     <div>
-  //       <h1>{props.position}</h1>
-  //       <h2>{props.company}</h2>
-  //       <h2>{props.salary}</h2>
-  //       <br/>
-  //     </div>
-  //   )
-  // }
---
+++ src/previous.js
@@ -0,0 +1,55 @@
+
+// Components & Props >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// return (
+//   // //     <div className="App">
+//   // //       <User name="Hamish" age={14} email="hamish@email.com" />
+//   // //       <User name="Lucy" age={15} email="lucy@email.com" />
+//   // //       <User name="Beth" age={13} email="beth@email.com" />
+//   // //     </div>
+//   // //   );
+//   // // }
+
+//   // // const User = (props) => {
+//   // //   return (
+//   // //     <div>
+//   // //       <h1>{props.name}</h1>
+//   // //       <h1>{props.age}</h1>
+//   // //       <h1>{props.email}</h1>
+//   // //     </div>
+//   // //   );
+
+//   //   return (
+//   //     <div className="App">
+//   //       <Job salary={90000} position="Senior SDE" company="Amazon" />
+//   //       <Job salary={12000} position="Junior SDE" company="Google" />
+//   //       <Job salary={10000} position="Project Manager" company="Netflix" />
+//   //     </div>
+//   //   );
+//   // };
+
+//   // const Job = (props) => {
+//   //   return (
+//   //     <div>
+//   //       <h1>{props.position}</h1>
+//   //       <h2>{props.company}</h2>
+//   //       <h2>{props.salary}</h2>
+//   //       <br/>
+//   //     </div>
+//   //   )
+//   // }
+
+
+// Conditional Rendering >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// function App() {
+//   const age = 14;
+//   const isGreen = true;
+
+//   return (
+//     <div className="App">
+//       {age >= 18 ? <h1>OVERAGE</h1> : <h1>UNDERAGE</h1>}
+//       <h1 style={{color: isGreen ? "green" : "red"}}>This has color</h1>
+
+//       {isGreen && <button>Here's a button</button>}
+//     </div>
+//   )
+// }
