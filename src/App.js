9f28f33a683d738c8281018ce724a1cf740590e3
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-18 10:28:49 +1030

    State function non working example

--- src/App.js
+++ src/App.js
@@ -3,35 +3,16 @@ import './App.css';
 import { Planet } from './Planet';
 
 function App() {
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-    { name: "Neptune", isGasPlanet: true },
-    { name: "Uranus", isGasPlanet: true },
-  ];
+  let age = 0;
 
-  return (
-    <div className="App">
-      {planets.map(
-        (planet, key) => planet.isGasPlanet && <h1> {planet.name} </h1>
-        )}
-    </div>
-  );
+  const increaseAge = () => {
+    age = age + 1
+    console.log(age);
+  };
+
+  return <div className="App">
+    {age}
+    <button onClick={increaseAge}> Increase Age</button>
+  </div>
 }
-
-// const users = [
-//   { name: "Dan", age: 29 },
-//   { name: "Dill", age: 28 },
-//   { name: "Adam", age: 30 },
-// ];
-
-//  return (
-//   <div className="App">
-//     {users.map((user, key) => {
-//       return <User name={user.name} age={user.age}/>
-//     })}
-//   </div>
-//  );
   export default App;
--- src/previous.js
+++ src/previous.js
@@ -53,3 +53,19 @@
 //     </div>
 //   )
 // }
+
+// const users = [
+//   { name: "Dan", age: 29 },
+//   { name: "Dill", age: 28 },
+//   { name: "Adam", age: 30 },
+// ];
+
+//  return (
+//   <div className="App">
+//     {users.map((user, key) => {
+//       return <User name={user.name} age={user.age}/>
+//     })}
+//   </div>
+//  );
+
+
