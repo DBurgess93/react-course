2675d62decd6f1fc672dba8d3d11af6441c570e6
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-24 00:17:31 +1030

    Add button change when task completed

--- src/Task.js
+++ src/Task.js
@@ -5,7 +5,11 @@ export const Task = (props) => {
     style={{ backgroundColor: props.completed ? "green" : "white" }}
     >
       <h1>{props.taskName}</h1>
-      <button onClick={() => props.completeTask(props.id)}>Complete</button>
+      <button onClick={() => props.completeTask(props.id)}
+        style={{ backgroundColor: props.completed ? "pink" : "default"}}
+      >
+        {props.completed ? "Completed" : "Complete"}
+      </button>
       <button onClick={() => props.deleteTask(props.id)}> Delete </button>
     </div>
   );
