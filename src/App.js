650e920e4423d69c58cf48f1dbf067630f79b9b3
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-24 17:16:39 +1030

    Add TaskApp.js file and import to App

--- src/App.js
+++ src/App.js
@@ -2,74 +2,16 @@ import './App.css';
 // import { User } from './User';
 // import { useState } from 'react';
 // import { useCounter } from './counter';
-import { useState } from "react";
-import { Task } from "./Task";
+// import { useState } from "react";
+// import { Task } from "./Task";
+import TaskApp from "./TaskApp";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  }
-
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    }
-    setTodoList([...todoList, task]);
-  };
-
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter(((task) =>  task.id !== id)))
-  };
-  // <<<<<< Long way >>>>>>
-  // const deleteTask = (taskName) => {
-  //   const newTodoList = todoList.filter((task) => {
-  //     if (task === taskName) {
-  //       return false;
-  //     } else {
-  //       return true;
-  //     }
-  //   });
-  //   setTodoList(newTodoList)
-  // };
-
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return { ...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
-
   return (
     <div className="App">
-      <div className="addTask">
-        <input onChange={handleChange}/>
-        <button onClick={addTask}>Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-          <Task
-            taskName={task.taskName}
-            id={task.id}
-            completed={task.completed}
-            deleteTask={deleteTask}
-            completeTask={completeTask}
-          />
-          );
-        })}
-      </div>
+      <TaskApp />
     </div>
-  );
+  )
 }
 
 export default App;
