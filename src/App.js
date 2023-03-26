a9341b7c0be1509d63b81e4ac22d063d85c2500a
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-27 09:56:35 +1030

    Exercise: Excuses API call - Add function to fetch data on click (not dynamic yet)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Axios from "axios";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 // import { User } from './User';
 // import { useCounter } from './counter';
 // import { useState } from "react";
@@ -10,29 +10,21 @@ import { useEffect, useState } from 'react';
 // import ToggleTextApp from "./ToggleTextApp"
 
 function App() {
-  const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
+  const [excuse, setExcuse] = useState("");
   const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      setPredictedAge(res.data)
-      console.log(res.data)
-    })
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/party/`).then((res) => {
+    setExcuse(res.data[0].excuse);
+      });
   };
 
   return (
     <div className="App">
-      <input
-        placeholder="your name..."
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <button onClick={fetchData}> Predict Age </button>
-      <h1> Name: {predictedAge?.name ? predictedAge?.name.charAt(0).toUpperCase()
-        + predictedAge?.name.slice(1) : ""}
-      </h1>
-      <h1> Predicted Age: {predictedAge?.age}</h1>
-      <h1> Count: {predictedAge?.count}</h1>
+      <h1> Generate an Excuse </h1>
+      <button onClick={fetchData}>Party</button>
+      <button>Family</button>
+      <button>Office</button>
+
+      <p> {excuse} </p>
     </div>
   );
 }
