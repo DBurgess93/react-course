f0c6b4102a67ac053050a77386c318342e897171
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-26 14:31:32 +1030

    Add Function fetchCatFact when button is clicked, using API

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( <App /> );
   // Strict mode not in use to stop double loading with API
-    <App />
-);
