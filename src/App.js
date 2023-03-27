6515d78b441f42b82d98a6567fceecababf53a61
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-27 16:45:36 +1030

    Add Navbar with Link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { Menu } from "./pages/Menu";
 import { Contact } from "./pages/Contact";
-
+import { Navbar } from "./pages/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/menu" element={<Menu />}/>
           <Route path="/contact" element={<Contact />}/>
+          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
         </Routes>
       </Router>
     </div>
