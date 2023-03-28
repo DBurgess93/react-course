bad79ed2515e87792ccc5b18f5b076255e8d5df8
Daniel Burgess <dan-burgess@hotmail.com>
2023-03-28 23:49:04 +1030

    Add react-router-dom psedo code to previous

--- src/previous.js
+++ src/previous.js
@@ -266,25 +266,42 @@
   //   </div>
   // );
 
+  // <<<<<< REACT ROUTER DOM >>>>>>
+  // import "./App.css";
+  // import React from 'react';
+  // import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+  // import { Home } from "./pages/Home";
+  // import { Menu } from "./pages/Menu";
+  // import { Contact } from "./pages/Contact";
+  // import { Navbar } from "./pages/Navbar";
 
+  // function App() {
+  //   return (
+  //     <div className="App">
+  //       <Router>
+  //         <Navbar />
+  //         <Routes>
+  //           <Route path="/" element={<Home />}/>
+  //           <Route path="/menu" element={<Menu />}/>
+  //           <Route path="/contact" element={<Contact />}/>
+  //           <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+  //         </Routes>
+  //       </Router>
+  //     </div>
+  //   );
+  // }
 
+  // export default App;
 
-import "./App.css";
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-}
-
-export default App;
+// <<<<<< Navbar for REAct Router dom >>>>>>
+// import React from 'react';
+// import { Link } from "react-router-dom";
+// export const Navbar = () => {
+//   return (
+//     <div>
+//       <Link to="/">Home</Link>
+//       <Link to="/menu">Menu</Link>
+//       <Link to="/contact">Contact</Link>
+//     </div>
+//   );
+// };
