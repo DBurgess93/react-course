dde681b53ef4a211102ec5faa86b5534512345f6
Daniel Burgess <dan-burgess@hotmail.com>
2023-04-03 13:46:45 +0930

    Delete all files and install/configure using Typescript

--- src/pages/Menu.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const Menu = () => {
-  return <h1>THIS IS THE MENU PAGE</h1>;
-}
--- src/pages/Profile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useContext } from "react";
-export const Profile = () => {
-  return (
-    <div>
-      Profile
-    </div>
-  );
-};
